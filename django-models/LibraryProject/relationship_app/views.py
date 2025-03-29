ea1ec9a41044dbbcd7d3689831402cc04a188dd5
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-29 17:19:21 +0200

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,7 @@ from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
+    "relationship_app/library_detail.html"
     model = Library
     template_name = "library_detail.html"
     context_object_name = "library"
