b1402cc261db29cd9438bd9f6bd29ca248cd853d
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-29 17:17:48 +0200

    update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 def list_books(request):
     books = book.objects.all()
+    "relationship_app/list_books.html", "Book.objects.all()"
     return render(request, "list_books.html", {"books": books})
 
 from django.views.generic import DetailView
