a62a5acac53da8e499cb38851070576abd1ac12b
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-29 20:53:27 +0200

    update

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
+    from django.apps import AppConfig
+
+class YourAppNameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'your_app_name'
+
+    def ready(self):
+        import your_app_name.signals  
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -43,3 +43,28 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, "register.html", {"form": form})
 "relationship_app/register.html"
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Role check functions
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
