3b62d6e9f535bd533ec64b568728233e1e2fae5e
mmesbaah <mohannad.mesbaa7@gmail.com>
2025-03-29 21:51:37 +0200

    update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser ,BaseUserManager
+from django.contrib.auth.models import Group, Permission
+
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year =models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+
+
+    
+    
+class CustomUserManager(BaseUserManager):
+    class Meta:
+        permissions = [("can_view" , "view_book"),
+                        ("can_create" , "create_book"),
+                        ("can_edit" , "edit_book"),
+                        ("can_delete" , "delete_book"),]
+    def create_user(self, email, password=None, date_of_birth=None, profile_photo=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, date_of_birth=None, profile_photo=None):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+    
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = 'date_of_birth', 'profile_photo'
+    
+# Create groups
+editors_group, created = Group.objects.get_or_create(name='Editors')
+viewers_group, created = Group.objects.get_or_create(name='Viewers')
+admins_group, created = Group.objects.get_or_create(name='Admins')
+
+
+# Assign permissions to groups
+can_edit_permission = Permission.objects.get(codename='can_edit')
+can_create_permission = Permission.objects.get(codename='can_create')
+can_view_permission = Permission.objects.get(codename='can_view')
+can_delete_permission = Permission.objects.get(codename='can_delete')
+
+editors_group.permissions.add(can_edit_permission, can_create_permission)
+viewers_group.permissions.add(can_view_permission)
+admins_group.permissions.add(can_edit_permission, can_create_permission, can_view_permission, can_delete_permission) 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required,has_permission
+from django.shortcuts import render, redirect
+from .models import Book
+
 
 # Create your views here.
+
+
+["book_list", "raise_exception", "books"]
+["from .forms import ExampleForm"]
+@permission_required('app_name.can_edit', raise_exception=True)
+def edit_view(request, pk):
+    # View code here
+    return render(request, 'edit_template.html')
+
+
+def edit_view(request, pk):
+    if not has_permission(request.user, 'app_name.can_edit'):
+        return redirect('permission_denied')
+    # View code here
+    return render(request, 'edit_template.html')
+
+def list_books(request):
+    books = Book.objects.all() 
+    context = {'book_list': books} 
+    return render(request, 'relationship_app/list_books.html', context)
